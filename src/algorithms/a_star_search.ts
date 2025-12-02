9d0c405fdbfe17472d5123025ac8c8d8ea808d3a
namnkahn1607 <nlnkandthebois@gmail.com>
2025-12-03 00:53:04 +0700

    A* Search add documentation and infos

--- src/algorithms/a_star_search.ts
+++ src/algorithms/a_star_search.ts
@@ -1,8 +1,15 @@
 /* advanced algorithms: A* Search */
-// shortest min-cost path in Matrix
+// a shortest path algorithm (Graph).
 import { MinPriorityQueue } from 'datastructures-js';
 
 class Astar {
+    /** A* Search shortestPath()
+     * A shortest path from the top left cell to the bottom right cell.
+     * - Allow diagonal traverse ? Chebyshev : Manhattan.
+     * - Property: Source-sink.
+     * @param {Array<Array<number>>} mat 
+     * @returns {number}
+     */
     shortestPath(mat: Array<Array<number>>): number {
         const len = mat.length;
         const dirs = [[-1, 0], [0, -1], [1, 0], [0, 1]];
@@ -11,19 +18,20 @@ class Astar {
             return -1;
 
         const heuristic = (pos: number[]) => {
-            return 2 * len - 2 - pos[0] - pos[1];
+            return 2 * len - 2 - pos[0] - pos[1]; // Manhattan distance
         };
 
         const opened = new MinPriorityQueue<number[]>(
             item => {
                 const [R, C, g] = item;
-                return g + heuristic([R, C]);
+                return g + heuristic([R, C]); // g(x) + h(x) = f(x)
             }
         );
         opened.enqueue([0, 0, 0]);
 
+        // 2 missions: Mark visited & Track cost (updatable).
         const gCost = new Map<string, number>();
-        gCost.set('0,0', 0);
+        gCost.set('0,0', 0.0);
 
         while (!opened.isEmpty()) {
             const [R, C, currG] = opened.dequeue()!;
@@ -33,6 +41,7 @@ class Astar {
             if (currG > gCost.get(currKey)!)
                 continue;
 
+            // Destination reached -> return g_cost.
             if (R == len - 1 && C == len - 1)
                 return currG;
 
@@ -40,18 +49,18 @@ class Astar {
                 const [newR, newC] = [R + dR, C + dC];
                 const neiKey = `${newR},${newC}`;
 
-                if (newR < 0 || newR >= len || 
-                    newC < 0 || newC >= len)
+                if (newR < 0 || newR >= len || newC < 0 || newC >= len)
                     continue;
 
-                if (mat[newR][newC] != 0)
+                if (mat[newR][newC] != 0) // blocked
                     continue;
 
                 const newG = currG + 1;
 
-                // Haven't computed OR find out a better path.
+                // Haven't computed OR find out a better path -> Relax.
                 if (!gCost.has(neiKey) || newG < gCost.get(neiKey)!) {
                     gCost.set(neiKey, newG);
+                    // Laziness: add new query instead of update priority key.
                     opened.enqueue([newR, newC, newG]);
                 }
             }
