227fc70dc74944a701aed3d1082eae6a887bf8c7
namnkahn1607 <nlnkandthebois@gmail.com>
2025-12-03 14:28:42 +0700

    Bipartite Graph

--- src/algorithms/bipartite.ts
+++ src/algorithms/bipartite.ts
@@ -1,9 +1,19 @@
 /* advanced algorithms: Bipartite */
-// check if bipartite Graph
+// check if Graph is bipartite.
 
 class Bipartite {
-    bipartite(n: number, edges: number[][], directed: boolean = false): boolean {
-        const adj = Array.from({ length: n }, () => new Array<number>());
+    /**
+     * @param {number} V 
+     * @param {Array<Array<number>>} edges 
+     * @param {boolean} directed 
+     * @returns {boolean}
+     */
+    bipartite(
+        V: number, edges: Array<Array<number>>, directed: boolean = false
+    ): boolean {
+        const adj = Array.from(
+            { length: V }, () => new Array<number>()
+        );
 
         for (const [u, v] of edges) {
             adj[u].push(v);
@@ -13,30 +23,35 @@ class Bipartite {
             }
         }
 
-        // black/white ~ true/false
-        const color = new Array(n).fill(false);
-        const visited = new Array(n).fill(false);
+        // Coloring: black -> true, white -> false.
+        const color = new Array(V).fill(false);
+        const visited = new Array(V).fill(false);
 
         const dfs = (curr: number): boolean => {
             visited[curr] = true;
 
             for (const nei of adj[curr]) {
                 if (visited[nei]) {
+                    // Visited & same color -> not bipartite.
                     if (color[nei] == color[curr])
                         return false;
 
                     continue;
                 }
 
+                // Color the opposite to all neighbors.
                 color[nei] = !color[curr];
                 
-                if (!dfs(nei)) return false;
+                // Already found a falsy clue -> return.
+                if (!dfs(nei)) {
+                    return false;
+                }
             }
 
             return true;
         }
         
-        for (let i = 0; i < n; ++i) {
+        for (let i = 0; i < V; ++i) {
             if (!visited[i] && !dfs(i)) {
                 return false;
             }
@@ -45,6 +60,10 @@ class Bipartite {
         return true;
     } 
 
+    /** Analysis of Complexity
+     * - Time: linear O(V + E) average/worst case.
+     * - Space: O(V + E).
+     */
     public static main(): void {}
 }
 
