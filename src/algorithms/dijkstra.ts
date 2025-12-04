c75f888b3cd67ba74d1320476a7e8acab79df2f3
namnkahn1607 <nlnkandthebois@gmail.com>
2025-12-04 16:53:56 +0700

    Lazy Dijkstra

--- src/algorithms/dijkstra.ts
+++ src/algorithms/dijkstra.ts
@@ -1,39 +1,62 @@
 /* algorithms: Dijkstra */
-// shortest mincost path in Matrix
+// a shortest path algorithm (Graph).
 import { MinPriorityQueue } from 'datastructures-js';
 
 class Dijkstra {
-    shortestPath(n: number, edges: number[][], src: number): number[] {
-        const adj = Array.from({ length: n }, () => new Array<number[]>());
-        const dists = new Array(n).fill(-1);
-
+    /** Dijkstra's shortestPath()
+     * Computes longest distances from source to all other vertices.
+     * - Property: Single source, Lazy.
+     * - Input criteria: Weighted Graph with no negative
+     * edge weights.
+     * @param {number} V 
+     * @param {Array<Array<number>>} edges 
+     * @param {number} src 
+     * @returns {Array<number>}
+     */
+    shortestPath(
+        V: number, edges: Array<Array<number>>, src: number
+    ): Array<number> {
+        const adj = Array.from(
+            { length: V }, () => new Array<number[]>()
+        );
+        
         for (const [u, v, w] of edges) {
             adj[u].push([v, w]);
         }
 
+        const distTo = new Array(V).fill(-1);
+        distTo[src] = 0.0;
         const minPQ = new MinPriorityQueue<number[]>(
-            item => item[0]
+            item => item[0], [[0, src]]
         );
-        minPQ.enqueue([0, src]);
-
+        
         while (!minPQ.isEmpty()) {
             const [w1, n1] = minPQ.dequeue()!;
 
-            if (dists[n1] != -1)
+            // Calculated -> skip.
+            if (distTo[n1] != -1)
                 continue;
 
-            dists[n1] = w1;
+            distTo[n1] = w1;
 
             for (const [n2, w2] of adj[n1]) {
-                if (dists[n2] != -1) {
+                // Laziness: add new query instead of update priority key.
+                if (distTo[n2] != -1) {
+                    /* Don't need to relax edge since new queries with better
+                    priority value get added anyway. */
                     minPQ.enqueue([w1 + w2, n2]);
                 }
             }
         }
 
-        return dists;
+        return distTo;
     }
 
+    /** Analysis of Complexity
+     * - Time: O(E.logV) average/worst case.
+     * - Space: O(V + E).
+     * -> Due to lazy querying, the PQ can get 'bloated' quickly.
+     */
     public static main(): void {}
 }
 
